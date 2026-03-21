26f904c624329a049bac03e79d5a4abe7e71f6a6
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2026-03-21 16:25:25 +0100

    Adding dummy test files

---
+++ OCR_Service/Worker/test.py
@@ -0,0 +1,2 @@
+def test_placeholder_python():
+    assert True, "Ez egy dummy Python teszt, mindig sikeres"
---
+++ Webservice/test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestPlaceholderGo(t *testing.T) {
+	t.Log("Ez egy dummy Go teszt, mindig sikeres")
+}
